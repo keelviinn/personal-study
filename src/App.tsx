13c0fc157e45045167e749459aeda7d4ec4fe9cb
Kelvin Oliveira <keelviinn@gmail.com>
2026-09-10 21:28:25 +0200

    chore: reset app to boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import './App.css';
-import { useFetchUser } from './hooks/useFetchUser';
 
 function App() {
-  const { user, isLoading: isUserLoading, error: userErrror } = useFetchUser();
-
-  console.log(user, userErrror);
-
   return (
-    <>
-      {isUserLoading && <span>User is loading...</span>}
-      {!isUserLoading && userErrror && <span>{userErrror.message}</span>}
-      {!isUserLoading && !userErrror && user && (
-        <>
-          <p>Hi {user.name}</p>
-        </>
-      )}
-
-      {/* <TransactionList /> */}
-    </>
+    <div className="app">
+      <h1>Personal Study</h1>
+      <p>Frontend interview preparation.</p>
+    </div>
   );
 }
 
