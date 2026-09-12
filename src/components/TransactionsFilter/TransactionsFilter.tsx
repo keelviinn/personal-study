a2c715b794a27fac7816a508f10c9cd25ec80b84
Kelvin Oliveira <keelviinn@gmail.com>
2026-09-12 12:22:45 +0200

    refactor: remove unnecessary form wrapper

--- src/components/TransactionsFilter/TransactionsFilter.tsx
+++ src/components/TransactionsFilter/TransactionsFilter.tsx
@@ -16,23 +16,21 @@ export function TransactionsFilter({
   onChangeState,
 }: TransactionsFilterProps) {
   return (
-    <form>
-      <fieldset>
-        <legend>Select transaction state</legend>
+    <fieldset>
+      <legend>Select transaction state</legend>
 
-        {TRANSACTION_STATES.map((transactionState) => (
-          <label key={transactionState}>
-            <input
-              type="radio"
-              name="state"
-              value={transactionState}
-              checked={state === transactionState}
-              onChange={() => onChangeState(transactionState)}
-            />
-            {transactionState}
-          </label>
-        ))}
-      </fieldset>
-    </form>
+      {TRANSACTION_STATES.map((transactionState) => (
+        <label key={transactionState}>
+          <input
+            type="radio"
+            name="state"
+            value={transactionState}
+            checked={state === transactionState}
+            onChange={() => onChangeState(transactionState)}
+          />
+          {transactionState}
+        </label>
+      ))}
+    </fieldset>
   );
 }
